def4067204332f85934f18f66a4b3a704c35d956
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-10 22:00:01 +0900

    다음해야할 문제 등록

--- solve.js
+++ solve.js
@@ -1,44 +1,8 @@
-const input = {
-    clothes: [
-        [
-            ["yellowhat", "headgear"],
-            ["bluesunglasses", "eyewear"],
-            ["green_turban", "headgear"],
-        ],
-        [
-            ["crowmask", "face"],
-            ["bluesunglasses", "face"],
-            ["smoky_makeup", "face"],
-        ],
-    ],
-    answer: [5, 3],
-};
+const input = {};
 
-function solution(clothes) {
-    const hash_map = {};
-    clothes.forEach(
-        (element) =>
-            (hash_map[element[1]] =
-                (hash_map[element[1]] ? hash_map[element[1]] : 0) + 1)
-    );
-    return (
-        Object.keys(hash_map).reduce((pre, value, idx, arr) => {
-            return pre * (hash_map[value] + 1);
-        }, 1) - 1
-    );
-}
+function solution() {}
 
-function test() {
-    for (let i = 0; i < 2; i++) {
-        const result = solution(input.clothes[i]);
-
-        if (result === input.answer[i]) {
-            console.log("O");
-        } else {
-            console.log("X");
-        }
-    }
-}
+function test() {}
 
 let start = new Date(); // 시작
 test();
