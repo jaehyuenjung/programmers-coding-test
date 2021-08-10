632073dfcfa9afe6a8efa3a2c7b306c83a20718c
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-10 21:58:09 +0900

    위장 풀이.js ver2 [2021/08/10]

--- solve.cpp
+++ solve.cpp
@@ -1,41 +1,7 @@
 #include<iostream>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <map>
 
 using namespace std;
-typedef vector<string> vs;
-typedef map<string, int> msi;
-typedef vector<vector<string>> vvs;
-typedef pair<string, int> psi;
 
-const vector<vector<vector<string>>> input= {
-    {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}},
-    {{"crowmask", "face"}, {"bluesunglasses", "face"}, {"smoky_makeup", "face"}},
-};
+int solution(){}
 
-const int answer[] = {5, 3};
-
-int solution(vector<vector<string>> clothes){
-    msi hash_map;
-    for_each(clothes.begin(), clothes.end(), [&](const vs &cloth)
-             { hash_map[cloth.back()] += 1; });
-    return accumulate(hash_map.begin(), hash_map.end(), 1, [&](int result, const psi &p){
-        return result * (hash_map[p.first] + 1);
-    }) - 1;
-}
-
-int main(){
-    for (int i = 0; i < input.size(); i++){
-        const int result = solution(input[i]);
-        if(result == answer[i]){
-            cout << "O"
-                 << "\n";
-        }else{
-            cout << "X"
-                 << "\n";
-        }
-    }
-}
+int main(){}
--- solve.js
+++ solve.js
@@ -1,8 +1,44 @@
-const input = {};
+const input = {
+    clothes: [
+        [
+            ["yellowhat", "headgear"],
+            ["bluesunglasses", "eyewear"],
+            ["green_turban", "headgear"],
+        ],
+        [
+            ["crowmask", "face"],
+            ["bluesunglasses", "face"],
+            ["smoky_makeup", "face"],
+        ],
+    ],
+    answer: [5, 3],
+};
 
-function solution() {}
+function solution(clothes) {
+    const hash_map = {};
+    clothes.forEach(
+        (element) =>
+            (hash_map[element[1]] =
+                (hash_map[element[1]] ? hash_map[element[1]] : 0) + 1)
+    );
+    return (
+        Object.keys(hash_map).reduce((pre, value, idx, arr) => {
+            return pre * (hash_map[value] + 1);
+        }, 1) - 1
+    );
+}
 
-function test() {}
+function test() {
+    for (let i = 0; i < 2; i++) {
+        const result = solution(input.clothes[i]);
+
+        if (result === input.answer[i]) {
+            console.log("O");
+        } else {
+            console.log("X");
+        }
+    }
+}
 
 let start = new Date(); // 시작
 test();
