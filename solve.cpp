ec7182f5c14aa2f1e35acf4531fdc8db26c97c95
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-13 12:43:37 +0900

    기능개발 풀이.js ver3 [2021/08/13]

--- solve.cpp
+++ solve.cpp
@@ -1,41 +1,7 @@
 #include<iostream>
-#include <string>
-#include <vector>
-#include <numeric>
-#include <algorithm>
 
 using namespace std;
 
-const vector<vector<int>> inputProgresses = {{93, 30, 55}, {95, 90, 99, 99, 80, 99}};
-const vector<vector<int>> inputSpeeds = {{1, 30, 5}, {1, 1, 1, 1, 1, 1}};
-const vector<vector<int>> answer = {{2, 1}, {1, 3, 2}};
+int solution() {}
 
-vector<int> solution(vector<int> progresses, vector<int> speeds) {
-    int max_day, idx;
-    max_day = idx = 0;
-    return accumulate(progresses.begin(), progresses.end(), vector<int>(), [&](vector<int> &result,const int value) {
-        int cur_day = (99 - progresses[idx]) / speeds[idx] + 1;
-        if(max_day < cur_day)
-            result.push_back(1);
-        else
-            ++result.back();
-        max_day = max(max_day, cur_day);
-        idx++;
-        return result;
-    });
-}
-
-int main(){
-    for (int i = 0; i < 2; i++){
-        vector<int> result = solution(inputProgresses[i], inputSpeeds[i]);
-        
-        string output = "O";
-        for (int j = 0; j < answer[i].size(); j++){
-            if(answer[i][j] != result[j]){
-                output = "X";
-            }
-        }
-
-        cout << output << "\n";
-    }
-}
+int main(){}
--- solve.js
+++ solve.js
@@ -1,8 +1,46 @@
-const input = {};
+const input = {
+    progresses: [
+        [93, 30, 55],
+        [95, 90, 99, 99, 80, 99],
+    ],
+    speeds: [
+        [1, 30, 5],
+        [1, 1, 1, 1, 1, 1],
+    ],
+    answer: [
+        [2, 1],
+        [1, 3, 2],
+    ],
+};
 
-function solution() {}
+function solution(progresses, speeds) {
+    let max_day = 0;
+    return progresses.reduce((result, value, idx) => {
+        const cur_day = parseInt((99 - progresses[idx]) / speeds[idx]) + 1;
+        if (max_day < cur_day) {
+            max_day = cur_day;
+            result.push(1);
+        } else {
+            ++result[result.length - 1];
+        }
+        return result;
+    }, []);
+}
 
-function test() {}
+function test() {
+    for (let i = 0; i < 2; i++) {
+        const result = solution(input["progresses"][i], input["speeds"][i]);
+        let output = "O";
+
+        for (let j = 0; j < input["answer"][i].length; j++) {
+            if (input["answer"][i][j] !== result[j]) {
+                output = "X";
+            }
+        }
+
+        console.log(output);
+    }
+}
 
 let start = new Date(); // 시작
 test();
