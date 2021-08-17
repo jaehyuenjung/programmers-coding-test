b20096ff8c86d8f6ea9d7f338f968e72444d2aba
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-18 01:05:54 +0900

    프린터 풀이.js [2021/08/18]

--- solve.js
+++ solve.js
@@ -1,8 +1,40 @@
-const input = {};
+const input = {
+    priorities: [
+        [2, 1, 3, 2],
+        [1, 1, 9, 1, 1, 1],
+    ],
+    location: [2, 0],
+    answer: [1, 5],
+};
 
-function solution() {}
+function solution(priorities, location) {
+    let result = 0;
+    let arr = priorities.map((priority, index) => ({ priority, index }));
+    while (true) {
+        const preEle = arr.shift();
+        if (arr.some((el) => el.priority > preEle.priority)) {
+            arr.push(preEle);
+        } else {
+            result++;
+            if (preEle.index == location) {
+                break;
+            }
+        }
+    }
+    return result;
+}
 
-function test() {}
+function test() {
+    for (let i = 0; i < 2; i++) {
+        const result = solution(input["priorities"][i], input["location"][i]);
+
+        if (input["answer"][i] === result) {
+            console.log("O");
+        } else {
+            console.log("X");
+        }
+    }
+}
 
 let start = new Date(); // 시작
 test();
