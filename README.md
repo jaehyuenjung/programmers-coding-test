300562b7150032fdd84add24a9a971a3e57ad10b
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-19 01:46:20 +0900

    다음해야할 문제 등록

--- README.md
+++ README.md
@@ -10,4 +10,5 @@ Coding test solution list
 -   [x] [베스트앨범](https://programmers.co.kr/learn/courses/30/lessons/42579)
 -   [x] [기능개발](https://programmers.co.kr/learn/courses/30/lessons/42586)
 -   [x] [프린터](https://programmers.co.kr/learn/courses/30/lessons/42587)
--   [ ] [다리를 지나는 트럭](https://programmers.co.kr/learn/courses/30/lessons/42583)
+-   [x] [다리를 지나는 트럭](https://programmers.co.kr/learn/courses/30/lessons/42583)
+-   [ ] [주식가격](https://programmers.co.kr/learn/courses/30/lessons/42584)
--- solve.js
+++ solve.js
@@ -1,51 +1,8 @@
-const input = {
-    bridge_length: [2, 100, 100],
-    weight: [10, 100, 100],
-    truck_weights: [
-        [7, 4, 5, 6],
-        [10],
-        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
-    ],
-    answer: [8, 101, 110],
-};
+const input = {};
 
-function solution(bridge_length, weight, truck_weights) {
-    let time = 0;
-    let total_weight = 0;
-    let queue = [];
+function solution() {}
 
-    queue.push({ weight: 0, loc: 0 });
-    while (queue.length || truck_weights.length) {
-        if (queue[0]["loc"] === time) total_weight -= queue.shift()["weight"];
-        if (total_weight + truck_weights[0] <= weight) {
-            total_weight += truck_weights[0];
-            queue.push({
-                weight: truck_weights.shift(),
-                loc: time + bridge_length,
-            });
-        } else {
-            if (queue[0]) time = queue[0]["loc"] - 1;
-        }
-        time++;
-    }
-    return time;
-}
-
-function test() {
-    for (let i = 0; i < 3; i++) {
-        const result = solution(
-            input["bridge_length"][i],
-            input["weight"][i],
-            input["truck_weights"][i]
-        );
-
-        if (result === input["answer"][i]) {
-            console.log("O");
-        } else {
-            console.log("X");
-        }
-    }
-}
+function test() {}
 
 let start = new Date(); // 시작
 test();
