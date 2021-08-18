bc984df6067aa7c3663c99babc9eebeead64d711
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-19 01:34:31 +0900

    다리를 지나는 트럭 풀이.js ver2 [2021/08/19]

--- solve.cpp
+++ solve.cpp
@@ -1,72 +1,7 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <queue>
 
 using namespace std;
 
-const vector<int> inputBridgeLength = {2, 100, 100};
-const vector<int> inputWeight = {10, 100, 100};
-const vector<vector<int>> inputTruckWeights = {{7, 4, 5, 6}, {10}, {10, 10, 10, 10, 10, 10, 10, 10, 10, 10}};
-const vector<int> answer = {8, 101, 110};
+int solution() {}
 
-class Truck{
-    public:
-        int idx;
-        int loc;
-        Truck(int idx, int loc){
-            this->idx = idx;
-            this->loc = loc;
-        }
-};
-
-int solution(int bridge_length, int weight, vector<int> truck_weights) {
-    int next_idx, total_weight, result;
-    next_idx = total_weight = result = 0;
-    vector<Truck*> trucks(truck_weights.size());
-    queue<Truck*> bridge;
-
-    for (int i = 0; i < trucks.size(); i++){
-        trucks[i] = new Truck(i,0);
-    }
-
-    while (next_idx < truck_weights.size() || !bridge.empty())
-    {
-        if (bridge.empty() && next_idx < truck_weights.size())
-        {
-             total_weight += truck_weights[next_idx];
-            bridge.push(trucks[next_idx]);
-            trucks[next_idx]->loc++;
-            next_idx++;
-        }
-        else{
-            Truck *fst_truck = bridge.front();
-            for (int i = 0; i < next_idx; i++)
-                trucks[i]->loc++;
-            if(fst_truck->loc > bridge_length){
-                total_weight -= truck_weights[fst_truck->idx];
-                bridge.pop();
-            }
-            if(next_idx < truck_weights.size() && total_weight + truck_weights[next_idx] <= weight){
-                total_weight += truck_weights[next_idx];
-                bridge.push(trucks[next_idx]);
-                trucks[next_idx]->loc++;
-                next_idx++;
-            }
-        }
-        result++;
-    }
-
-    return result;
-}
-
-int main(){
-    for (int i = 0; i < 3; i++){
-        int result = solution(inputBridgeLength[i], inputWeight[i], inputTruckWeights[i]);
-        if(result == answer[i])
-            cout << "O" << "\n";
-        else
-            cout << "X" << "\n";
-        cout << result << "\n";
-    }
-}
+int main(){}
--- solve.js
+++ solve.js
@@ -1,8 +1,67 @@
-const input = {};
+const input = {
+    bridge_length: [2, 100, 100],
+    weight: [10, 100, 100],
+    truck_weights: [
+        [7, 4, 5, 6],
+        [10],
+        [10, 10, 10, 10, 10, 10, 10, 10, 10, 10],
+    ],
+    answer: [8, 101, 110],
+};
 
-function solution() {}
+function solution(bridge_length, weight, truck_weights) {
+    let time = 0;
+    let total_weight = 0;
+    let queue = [];
+    const trucks = truck_weights.map((weight, idx) => ({
+        idx,
+        weight,
+        loc: 0,
+        is_gone: false,
+    }));
+    while (trucks.some((el) => !el["is_gone"])) {
+        const truck = trucks.find((el) => !el["is_gone"] && !el["loc"]);
+        queue = queue.map((el) => {
+            ++el["loc"];
+            return el;
+        });
 
-function test() {}
+        if (queue.length && queue[0]["loc"] > bridge_length) {
+            const remove_truck = queue.shift();
+            total_weight -= remove_truck["weight"];
+            trucks[remove_truck["idx"]]["is_gone"] = true;
+        }
+        if (
+            trucks.some((el) => !el["is_gone"]) &&
+            truck &&
+            (queue.length == 0 || total_weight + truck["weight"] <= weight)
+        ) {
+            total_weight += truck["weight"];
+            truck["loc"]++;
+            queue.push(truck);
+        }
+
+        time++;
+    }
+
+    return time;
+}
+
+function test() {
+    for (let i = 0; i < 3; i++) {
+        const result = solution(
+            input["bridge_length"][i],
+            input["weight"][i],
+            input["truck_weights"][i]
+        );
+
+        if (result === input["answer"][i]) {
+            console.log("O");
+        } else {
+            console.log("X");
+        }
+    }
+}
 
 let start = new Date(); // 시작
 test();
