c97971baec212acc0c79eb7896d4ad1ef6a74ed3
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-19 20:34:15 +0900

    주식가격 풀이.js ver2 [2021/08/19]

--- solve.cpp
+++ solve.cpp
@@ -1,43 +1,7 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
 using namespace std;
 
-const vector<int> inputPrices = {1, 2, 3, 2, 3};
-const vector<int> outputReturn = {4, 3, 1, 1, 0};
+int solution() {}
 
-vector<int> solution(vector<int> prices) {
-    vector<int> time_stamp(prices.size());
-    vector<int> answer(prices.size());
-    time_stamp[prices.size() - 1] = prices.size();
-    answer[prices.size() - 1] = 0;
-    for (int i = prices.size() - 2; i >= 0; i--)
-    {
-        if(prices[i] <= prices[i+1]){
-            answer[i] = answer[i+1] + 1;
-            time_stamp[i] = time_stamp[i+1];
-            while(time_stamp[i] < prices.size() && prices[i] <= prices[time_stamp[i]]){
-                answer[i] += answer[time_stamp[i]];
-                time_stamp[i] = time_stamp[time_stamp[i]];
-            }
-        }else{
-            answer[i] = 1;
-            time_stamp[i] = i + 1;
-        }
-    }
-    return answer;
-}
-
-int main(){
-    const vector<int> result = solution(inputPrices);
-
-    string output = "O";
-    for (int i = 0; i < outputReturn.size(); i++){
-        if(result[i] != outputReturn[i]){
-            output = "X";
-        }
-        cout << result[i] << "\n";
-    }
-    cout << output << "\n";
-}
+int main(){}
--- solve.js
+++ solve.js
@@ -1,8 +1,44 @@
-const input = {};
+const input = {
+    prices: [1, 2, 3, 2, 3],
+    return: [4, 3, 1, 1, 0],
+};
 
-function solution() {}
+function solution(prices) {
+    time_stamp = [];
+    answer = [];
+    time_stamp[prices.length - 1] = prices.length;
+    answer[prices.length - 1] = 0;
+    for (let i = prices.length - 2; i >= 0; i--) {
+        if (prices[i] <= prices[i + 1]) {
+            answer[i] = answer[i + 1] + 1;
+            time_stamp[i] = time_stamp[i + 1];
+            while (
+                time_stamp[i] < prices.length &&
+                prices[i] <= prices[time_stamp[i]]
+            ) {
+                answer[i] += answer[time_stamp[i]];
+                time_stamp[i] = time_stamp[time_stamp[i]];
+            }
+        } else {
+            answer[i] = 1;
+            time_stamp[i] = i + 1;
+        }
+    }
+    return answer;
+}
 
-function test() {}
+function test() {
+    const result = solution(input["prices"]);
+
+    let output = "O";
+    for (let i = 0; i < input["return"].length; i++) {
+        if (input["return"][i] !== result[i]) {
+            output = "X";
+            break;
+        }
+    }
+    console.log(output);
+}
 
 let start = new Date(); // 시작
 test();
