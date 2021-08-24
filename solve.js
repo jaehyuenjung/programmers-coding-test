86e16d8f06e187718125b41a4508556aac8fa072
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-24 18:39:08 +0900

    K번째수 풀이.js ver2 [2021/08/24]

--- solve.js
+++ solve.js
@@ -1,8 +1,31 @@
-const input = {};
+const input = {
+    array: [1, 5, 2, 6, 3, 7, 4],
+    commands: [
+        [2, 5, 3],
+        [4, 4, 1],
+        [1, 7, 3],
+    ],
+    return: [5, 6, 3],
+};
 
-function solution() {}
+function solution(array, commands) {
+    const answer = [];
+    commands.forEach((el) => {
+        answer.push(array.slice(--el[0], el[1]).sort((a, b) => a - b)[--el[2]]);
+    });
+    return answer;
+}
 
-function test() {}
+function test() {
+    const result = solution(input["array"], input["commands"]);
+    let output = "O";
+    for (let i = 0; i < input["return"].length; i++) {
+        if (input["return"][i] !== result[i]) {
+            output = "X";
+        }
+    }
+    console.log(output);
+}
 
 let start = new Date(); // 시작
 test();
