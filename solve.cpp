32b8ad276faf75ca8f38e8b820027684986e9aef
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-24 18:27:22 +0900

    K번째수 풀이.cpp ver1 [2021/08/24]

--- solve.cpp
+++ solve.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int solution() {}
+const vector<int> inputArray = {1, 5, 2, 6, 3, 7, 4};
+const vector<vector<int>> commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+const vector<int> ouputReturn = {5, 6, 3};
 
-int main(){}
+vector<int> solution(vector<int> array, vector<vector<int>> commands) {
+    vector<int> answer;
+    for(vector<int> comm : commands){
+        vector<int> tmp;
+        copy(array.begin() + --comm[0], array.begin() + comm[1],  back_inserter(tmp));
+        sort(tmp.begin(), tmp.end());
+        answer.push_back(tmp[--comm[2]]);
+    }
+    return answer;
+}
+
+int main(){
+    const vector<int> result = solution(inputArray, commands);
+    string output = "O";
+    for (int i = 0; i < ouputReturn.size(); i++){
+        if(ouputReturn[i] != result[i]){
+            output = "X";
+            break;
+        }
+    }
+    cout << output << "\n";
+}
