f3de736dcfdd41c7f677d525e5f87c993553f60d
jaehyuenjung <jaehyuen0613@gmail.com>
2021-08-25 21:33:40 +0900

    가장 큰 수 풀이.js ver2 [2021/08/25]

--- solve.cpp
+++ solve.cpp
@@ -1,42 +1,7 @@
 #include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <numeric>
 
 using namespace std;
 
-const vector<vector<int>> inputNumbers = {{6, 10, 2}, {3, 30, 34, 5, 9}, {90, 908, 89, 898, 10, 101, 1, 8, 9}};
-const vector<string> outputReturn = {"6210", "9534330", "990908898988110110"};
+int solution() {}
 
-struct cmp{
-    const bool operator()(const string &a, const string &b){
-        string va = a + b;
-        string vb = b + a;
-        return va > vb;
-    }
-};
-
-string solution(vector<int> numbers) {
-    vector<string> array;
-    for(auto num : numbers)
-        array.push_back(to_string(num));
-    sort(array.begin(), array.end(), cmp());
-    if(array.front().compare("0")==0)
-        return "0";
-    return accumulate(array.begin(), array.end(), string(""), [](string result, string value) {
-        return result + value;
-    });
-}
-
-int main(){
-    for (int i = 0; i < 3; i++){
-        const string result = solution(inputNumbers[i]);
-        if(outputReturn[i].compare(result) == 0){
-            cout << "O" << "\n";
-        }else{
-            cout << "X" <<"\n";
-        }
-        cout << result << "\n";
-    }
-}
+int main(){}
--- solve.js
+++ solve.js
@@ -1,8 +1,30 @@
-const input = {};
+const input = {
+    numbers: [
+        [6, 10, 2],
+        [3, 30, 34, 5, 9],
+    ],
+    return: ["6210", "9534330"],
+};
 
-function solution() {}
+function solution(numbers) {
+    numbers = numbers.map((el) => String(el));
+    numbers = numbers.sort((a, b) => b + a - (a + b));
+    console.log(numbers);
+    if (numbers[0] === "0") return "0";
+    return numbers.reduce((result, value) => result + value, "");
+}
 
-function test() {}
+function test() {
+    for (let i = 0; i < 2; i++) {
+        const result = solution(input["numbers"][i]);
+        if (input["return"][i] === result) {
+            console.log("O");
+        } else {
+            console.log("X");
+        }
+        console.log(result);
+    }
+}
 
 let start = new Date(); // 시작
 test();
